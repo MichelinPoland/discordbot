5b875b3daf913069db766cef97e3f792126c6adc
DanIsNearby.eth <91475837+DanIsNearby@users.noreply.github.com>
2023-12-11 23:05:58 +0100

    test

--- commands/Info/wallet.js
+++ commands/Info/wallet.js
@@ -53,7 +53,7 @@ async function fetchWalletInfo(interaction, address) {
         // Iterate through the "nfts" array and send information for each item
         for (const nft of nftData.nfts) {
             const id = nft.identifier;
-            console.log(nftData.total)
+            console.log(openSeaResponse.total)
             const price = await fetchbibPrice(id);
             const name = nft.name;
             const description = nft.description;
